2799b2cb87f7c9a77bbb7dac8db6bf32a8aaa235
Christian Dean <christiandeandemesa@gmail.com>
2022-12-06 12:18:28 -0800

    Custom MUI theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {CssBaseline, ThemeProvider} from '@mui/material';
+
+import {ColorModeContext, useMode} from './theme';
+
 function App() {
-	return <div className='app'></div>;
+	const [theme, colorMode] = useMode();
+
+	return (
+		// Our created context.
+		<ColorModeContext.Provider value={colorMode}>
+			{/* MUI's context for its theme */}
+			<ThemeProvider theme={theme}>
+				{/* MUI's built-in CSS reset */}
+				<CssBaseline />
+				<div className='app'>
+					<main className='content'></main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
